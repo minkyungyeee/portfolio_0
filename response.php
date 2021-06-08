573e5d50797eba10d9a08ab83bcf0b33ec74550d
minkyungyeee <minkfree94@gmail.com>
2021-06-08 12:06:55 +0900

    Update php

--- response.php
+++ response.php
@@ -1,23 +1,21 @@
 <?php
-include_once('header.php');
-//데이터베이스 헤더문() 
-//서버이름(localhost)
-//사용자이름()
-//비밀번호()
-//데이터베이스이름 설정
+$servername = 'localhost';
+$username   = 'minkfree94';
+$password   = 'alsrud1045!';
+$dbname     = 'minkfree94';
 
-//폼 정보를 변수설정 그리고 저장
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+mysqli_set_charset($conn,'utf8');
+
 $email = $_POST['email'];
 $code = $_POST['code'];
 
 //DB내에 TABLE(email)에 입력하는 SQL 코딩
 
-//데이터베이스에 저장(입력)
-//sql insert into $email
 $sql = "insert into email_table (email) values ('$email')";
 $result = mysqli_query($conn,$sql);
 
 //서버(server)가 클라이언트(client)에 응답
-echo '<p>축하합니다. 매일 리스트에 저장되었습니다.</p>'.$email.'<br>'.$code; /* .이 문자열과 변수를 이어주는역할 */
-include_once('footer.php');
+echo '<p>축하합니다. 매일 리스트에 저장되었습니다.</p>'.$email; /* .이 문자열과 변수를 이어주는역할 */
+//include_once('footer.php');
 ?>
